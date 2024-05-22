bc01d95a37f7e65e2872ddb7e2dcde675f7c3494
Julien Castets <castets.j@gmail.com>
2024-05-22 17:39:07 +0200

    Wait rnd seconds

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import time
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Koyeb, xxx'
+    rnd = random.randrange(0, 5)
+    time.sleep(rnd)
+    return f'Hello from Koyeb! After {rnd} seconds :)'
 
 
 if __name__ == "__main__":
